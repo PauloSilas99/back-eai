198f76dfb5b4f92520b064926d68b1e367fb35e4
PauloSilas99 <fonsecasodreps@hotmail.com>
2025-06-27 15:58:17 -0300

    integracao vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8080', // Porta do seu Vite/React
+    //origin: 'http://localhost:8080', // Porta do seu Vite/React
+    origin: 'https://estudai-ten.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
