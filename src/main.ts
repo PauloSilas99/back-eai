61b774415a45f6ece87598052ae52772948cd639
PauloSilas99 <fonsecasodreps@hotmail.com>
2025-06-27 16:23:33 -0300

    ajuste no main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     //origin: 'http://localhost:8080', // Porta do seu Vite/React
-    origin: 'https://estudai-ten.vercel.app/',
+    origin: 'https://estudai-ten.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
